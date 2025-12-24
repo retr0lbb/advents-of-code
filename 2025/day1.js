8abd2e80712ae225b6b88b2fb410b67a6c2d14ee
retr0lbb <retr0lbb@gmail.com>
2025-12-24 00:58:57 -0300

    day 1 part 2 complete

--- 2025/day1.js
+++ 2025/day1.js
@@ -1,6 +1,6 @@
 import {read_input_file} from "../utils/read_input_file.js"
 
-function main(){
+function part1(){
     const inputs = read_input_file("./2025/inputs/part1.txt")
     console.log(inputs)
 
@@ -20,8 +20,6 @@ function main(){
             pointer = (pointer + turnNumber) % 100
         }
 
-        console.log("Pointer: ", pointer)
-
         if(pointer === 0){
             zeroCounter += 1
         }
@@ -29,4 +27,58 @@ function main(){
 
     console.log(zeroCounter)
 }
-main()
+
+function part2() {
+    const inputs = read_input_file("./2025/inputs/part1.txt")
+
+    let zeroCounter = 0
+    let pointer = 50
+
+    function spin(dial, rot){
+        const start = dial
+        const end = ((dial + rot) % 100 + 100) % 100
+        
+        let revolutions = 0
+        
+        if(rot > 0) {
+            if(start === 0) {
+                if(rot >= 100) {
+                    revolutions = Math.floor(rot / 100)
+                }
+            } else if(start + rot >= 100) {
+                revolutions = Math.floor((start + rot) / 100)
+            }
+        } else if(rot < 0) {
+            if(start === 0) {
+                if(Math.abs(rot) >= 100) {
+                    revolutions = Math.floor(Math.abs(rot) / 100)
+                }
+            } else if(start + rot < 0) {
+                revolutions = Math.floor(Math.abs(start + rot) / 100) + 1
+            }
+        }
+        
+        console.log(`inputs(${dial}, ${rot}) -> new Dial: ${end}, passagens por 0: ${revolutions}`)
+        
+        return {newDial: end, rev: revolutions}
+    }
+
+    inputs.forEach(str => {
+        const isL = str.startsWith("L")
+        const turnNumber = Number.parseInt(str.slice(1))
+
+        const rotation = isL ? -turnNumber : turnNumber
+        const {newDial, rev} = spin(pointer, rotation)
+        
+        zeroCounter += rev
+        
+        if(newDial === 0 && rev === 0){
+            zeroCounter++
+        }
+        
+        pointer = newDial
+    })
+
+    console.log(`Total de passagens/paradas no zero: ${zeroCounter}`)
+}
+part2()
