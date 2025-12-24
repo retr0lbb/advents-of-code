973ac34e0a9889e70fb3e5c60b4b14cbc15260f7
retr0lbb <retr0lbb@gmail.com>
2025-12-23 23:40:42 -0300

    day 1 part 1 complete

--- 2025/day1.js
+++ 2025/day1.js
@@ -1,3 +1,32 @@
 import {read_input_file} from "../utils/read_input_file.js"
 
-console.log(read_input_file("./2025/inputs/examples.txt"))
+function main(){
+    const inputs = read_input_file("./2025/inputs/part1.txt")
+    console.log(inputs)
+
+    let zeroCounter = 0;
+    const pointerStartPosition = 50;
+
+    let pointer = pointerStartPosition
+
+    inputs.map(str => {
+        const isL = str.startsWith("L") ? true : false
+        
+        const turnNumber = Number.parseInt(str.slice(1))
+
+        if (isL) {
+            pointer = (pointer - turnNumber + 100) % 100
+        } else {
+            pointer = (pointer + turnNumber) % 100
+        }
+
+        console.log("Pointer: ", pointer)
+
+        if(pointer === 0){
+            zeroCounter += 1
+        }
+    })
+
+    console.log(zeroCounter)
+}
+main()
