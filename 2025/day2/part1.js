9626bb51ea74fee63d12ffff81463c2a0dda6df6
retr0lbb <retr0lbb@gmail.com>
2025-12-25 00:05:01 -0300

    day 2 part 2 done

--- 2025/day2/part1.js
+++ 2025/day2/part1.js
@@ -7,7 +7,7 @@ function part1(){
 
     ranges.forEach(range => {
 
-    const [start, finish] = range.split("-")
+        const [start, finish] = range.split("-")
 
         for (let i = Number(start); i <= Number(finish); i++) {
             const str = i.toString()
@@ -28,4 +28,44 @@ function part1(){
     console.log(sumOfAll)
 }
 
-part1()
+
+function part2() {
+    const inputs = read_input_file_as_line("./inputs/input.txt");
+    const ranges = inputs.split(",");
+    let sumOfAll = 0;
+
+    ranges.forEach(range => {
+        const [start, finish] = range.split("-");
+
+        for (let num = Number(start); num <= Number(finish); num++) {
+            const str = num.toString();
+            const len = str.length;
+
+            let isInvalid = false;
+
+            for (let size = 1; size <= len / 2; size++) {
+
+                if (len % size !== 0) continue;
+
+                const times = len / size;
+                if (times < 2) continue;
+
+                const pattern = str.slice(0, size);
+                const rebuilt = pattern.repeat(times);
+
+                if (rebuilt === str) {
+                    isInvalid = true;
+                    break;
+                }
+            }
+
+            if (isInvalid) {
+                sumOfAll += num;
+            }
+        }
+    });
+
+    console.log(sumOfAll);
+}
+
+part2();
